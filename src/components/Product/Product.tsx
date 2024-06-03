51093eda630a7c9a279c09229c801078cc4e310e
jpaugusto13 <jpaugusto13@proton.me>
2024-06-03 13:14:37 -0300

    Add Carousel

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,19 +1,20 @@
 interface ProductProps {
   name: string;
   price: number;
+  image: string;
 }
 
-export default function Product({ name, price }: ProductProps) {
+export default function Product({ name, price, image }: ProductProps) {
   return (
-    <div className="w-[250px] shadow-md rounded-lg p-4 cursor-pointer hover:bg-gray-100 duration-300">
+    <div className="w-[350px] shadow-md rounded-lg p-4 cursor-pointer hover:bg-gray-100 duration-300">
       <img
-        className="rounded-xl  h-[100%] w-[100%] object-cover"
-        src="blusa.jpeg"
+        className="rounded-xl h-[350px] w-[350px] object-cover"
+        src={image}
       />
       <div className="flex flex-col w-[100%] p-2">
         <p>{name}</p>
         <div>
-          <p className="text-lg">R$ {price}</p>
+          <p className="text-lg">R$ {price.toFixed(2).replace(".", ",")}</p>
         </div>
       </div>
     </div>
--- src/components/Storage/Storage.tsx
+++ src/components/Storage/Storage.tsx
@@ -4,18 +4,23 @@ import "react-alice-carousel/lib/alice-carousel.css";
 
 export default function Storage() {
   return (
-    <div>
-      <AliceCarousel mouseTracking>
-        <Link href="#">
-          <img src="path-to-image" />
-        </Link>
-      </AliceCarousel>
+    <div className="flex flex-col justify-center">
+      <div className="w-full flex items-center p-10">
+        <AliceCarousel mouseTracking infinite>
+          <Link href="#">
+            <img width={400} src="image1.jpg" />
+          </Link>
+          <Link href="#">
+            <img width={500} height={300} src="image2.jpg" />
+          </Link>
+        </AliceCarousel>
+      </div>
 
       <div className="grid justify-center place-items-center p-10 grid-cols-4">
-        <Product name="Blusa Oversized lisa" price={75} />
-        <Product name="Blusa Oversized lisa" price={75} />
-        <Product name="Blusa Oversized lisa" price={75} />
-        <Product name="Blusa Oversized lisa" price={75} />
+        <Product image="blusa.jpeg" name="Blusa Oversized lisa" price={75} />
+        <Product image="1.jpg" name="Blusa Oversized lisa" price={75} />
+        <Product image="2.jpg" name="Blusa Oversized lisa" price={75} />
+        <Product image="3.jpg" name="Blusa Oversized lisa" price={75} />
       </div>
     </div>
   );
